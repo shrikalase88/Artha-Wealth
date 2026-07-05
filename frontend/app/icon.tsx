946f6e59264afdf029627cbdbe19561dff1331ca
shrikalase <shrikalase88@gmail.com>
2026-07-05 21:42:36 +0530

    feat(ui): add dynamic app icon and favicon matching Artha Wealth design

---
+++ frontend/app/icon.tsx
@@ -0,0 +1,29 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+export const size = { width: 32, height: 32 }
+export const contentType = 'image/png'
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: '#0B1527',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <svg width="24" height="24" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M 512 150 L 150 850 L 350 850 L 512 537 L 674 850 L 874 850 Z" fill="white"/>
+          <path d="M 180 650 Q 350 650 512 500 T 800 250" stroke="white" strokeWidth="100" strokeLinecap="round" fill="none"/>
+          <path d="M 600 250 L 800 250 L 800 450" stroke="white" strokeWidth="100" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+        </svg>
+      </div>
+    ),
+    { ...size }
+  )
+}
